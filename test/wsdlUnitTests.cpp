7be1cc04ecd008202f23091408b3929d8c2566c8
Ben M. Ward <ben.ward@gmail.com>
2014-12-30 16:57:33 -0700

    Updated tests

---
+++ test/wsdlUnitTests.cpp
@@ -0,0 +1,13 @@
+// wsdlUnitTests.cpp
+#include "../wsdl.h"
+#include <gtest/gtest.h>
+
+TEST(WSDLTest, WSDKLoadOperationCount) { 
+//   XSD xsd("testData/basic.xsd");
+//   ASSERT_EQ(4, xsd.get_ElementNames().size());
+}
+
+int main(int argc, char **argv) {
+   testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
--- test/xsdUnitTests.cpp
+++ test/xsdUnitTests.cpp
@@ -1,10 +1,15 @@
-// tests.cpp
+// xsdUnitTests.cpp
 #include "../xsd.h"
 #include <gtest/gtest.h>
 
 TEST(XSDTest, XSDLoadElementCount) { 
    XSD xsd("testData/basic.xsd");
    ASSERT_EQ(4, xsd.get_ElementNames().size());
+}
+
+TEST(XSDTest, XSDLoadSimpleTypeCount) { 
+   XSD xsd("testData/basic.xsd");
+   ASSERT_EQ(1, xsd.get_SimpleTypeNames().size());
 }
 
 int main(int argc, char **argv) {
