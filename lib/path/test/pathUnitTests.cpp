85c89456218188ef1f36a7e1de927358817a1892
Ben M. Ward <ben.ward@gmail.com>
2017-02-19 19:57:10 -0800

    fixed issue 15

--- lib/path/test/pathUnitTests.cpp
+++ lib/path/test/pathUnitTests.cpp
@@ -33,6 +33,21 @@ TEST(PATHTest, PathUNC) {
    ASSERT_EQ(string("file://") + Path::CurrentDirectory() + "data/foo.txt", path.get_UNC());
 }
 
+TEST(PATHTest, PathIsUNCNoPath) {
+   Path path("foo.txt");
+   EXPECT_FALSE(path.is_UNC()) << "\"foo.txt\" was an UNC";
+   path = "data/foo.txt";
+   EXPECT_FALSE(path.is_UNC()) << "\"data/foo.txt\" was an UNC";
+   path = "/home/user/data/foo.txt";
+   EXPECT_FALSE(path.is_UNC()) << "\"/home/user/data/foo.txt\" was an UNC";
+   path = "file:///home/user/data/foo.txt";
+   EXPECT_TRUE(path.is_UNC()) << "\"file:///home/user/data/foo.txt\" was not an UNC";
+   path = "http://example.com/data/foo.txt";
+   EXPECT_TRUE(path.is_UNC()) << "\"http://example.com/data/foo.txt\" was not an UNC";
+   path = "https://example.com/data/foo.txt";
+   EXPECT_TRUE(path.is_UNC()) << "\"https://example.com/data/foo.txt\" was not an UNC";
+}
+
 TEST(PATHTest, PathAbsolute) {
    Path path("foo");
    
