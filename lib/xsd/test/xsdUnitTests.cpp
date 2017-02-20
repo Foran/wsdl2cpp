83e3541107b9b7fb4d23dbe16da4ba2999e24a70
Ben M. Ward <ben.ward@gmail.com>
2017-02-19 18:16:44 -0800

    Ammended Paths

--- lib/xsd/test/xsdUnitTests.cpp
+++ lib/xsd/test/xsdUnitTests.cpp
@@ -3,13 +3,13 @@
 #include "gtest/gtest.h"
 
 TEST(XSDTest, XSDLoadElementCount) { 
-   XSD xsd("test/testData/basic.xsd");
+   XSD xsd("data/basic.xsd");
    //TODO(foran): fix test
    //EXPECT_EQ((unsigned int)4, xsd.get_ElementNames().size());
 }
 
 TEST(XSDTest, XSDLoadSimpleTypeCount) { 
-   XSD xsd("test/testData/basic.xsd");
+   XSD xsd("data/basic.xsd");
    //TODO(foran): fix test
    //EXPECT_EQ((unsigned int)1, xsd.get_SimpleTypeNames().size());
 }
